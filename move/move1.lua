eb05ed7cbb45e92bb7c991db2b3c9098c0351818
Yuki-Shibanuma <smatosan@gmail.com>
2018-05-02 15:15:27 +0900

    move

---
+++ move/move1.lua
@@ -0,0 +1,36 @@
+# ===================
+# コンピュータクラフト
+# 基本操作系
+# ===================
+
+# 前に進む
+turtle.forward()
+
+# 後ろに戻る
+turtle.back()
+
+# 右に回転
+turtle.right()
+
+# 左に回転
+turtle.left()
+
+# 上に移動
+turtle.up()
+
+# 下に移動
+turtle.down()
+
+# 前を掘る
+turtle.dig()
+
+# 上を掘る
+turtle.digUp()
+
+# 下を掘る
+turtle.digDown()
+
+# 繰り返し
+for i=1,16 do
+  turtle.forward()
+end
